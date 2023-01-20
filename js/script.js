3ec77b900335385bcf0a9199f7bf6b7d7aaabd5c
leah-olorunfemi <leahfemi@gmail.com>
2023-01-21 00:28:00 +0100

    Changed name of reindex , recss and rescripts to main files and
    Changed name of reindex , recss and rescripts to main files and changed previous index, css and js  scripts files to trial files.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // alert("hey");
 
+// Thursday January 16th, 2022 03:12
 function formatDay() {
-  let currentTime = new Date();
+  let currentDate = new Date();
   let days = [
     "Sunday",
     "Monday",
@@ -11,7 +12,9 @@ function formatDay() {
     "Friday",
     "Saturday",
   ];
-  let currentDay = days[currentTime.getDay()];
+
+  let day = days[currentDate.getDay()];
+  // console.log(day);
 
   let months = [
     "January",
@@ -28,92 +31,127 @@ function formatDay() {
     "December",
   ];
 
-  let currentMonth = months[currentTime.getMonth()];
-  let currentDate = currentTime.getDate();
-  let currentYear = currentTime.getFullYear();
-  return `${currentDay} ${currentMonth} ${currentDate}, ${currentYear}`;
+  let month = months[currentDate.getMonth()];
+  let date = currentDate.getDate();
+  // console.log(date);
+  let year = currentDate.getFullYear();
+  return `${day} ${month} ${date},${year}`;
 }
+
+let dateDisplayed = document.querySelector("#day-info");
+dateDisplayed.innerHTML = formatDay();
 
 function formatDate() {
-  let currentTime = new Date();
-  let currentHours = currentTime.getHours();
-  if (currentHours < 0) currentHours = `0${currentHours}`;
-  let currentMins = currentTime.getMinutes();
-  if (currentMins < 0) currentMins = `0${currentMins}`;
-  return `Time: <i class="fa-regular fa-clock clock"> ${currentHours}:${currentMins}`;
+  let currentDate = new Date();
+  let hours = currentDate.getHours();
+  if (hours < 0) {
+    hours = `0${hours}`;
+  }
+  let mins = currentDate.getMinutes();
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+  return `${hours}:${mins}`;
 }
 
-let dayElement = document.querySelector("#day-month");
-dayElement.innerHTML = formatDay();
-
-let timeElement = document.querySelector("#time");
-timeElement.innerHTML = formatDate();
+let timeShown = document.querySelector("#time");
+timeShown.innerHTML = formatDate();
 
 function displayWeather(response) {
   // console.log(response.data);
-  let cityElement = document.querySelector("#city-name");
-  let cityDisplayed = response.data.name;
-  cityElement.innerHTML = cityDisplayed;
 
-  //   To get name of city = console.log(response.data.name);
-
-  //   let temperatureElememt = document.querySelector("#temp");
-  //   let tempDisplayed = Math.round(response.data.main.temp);
-  //   temperatureElememt.innerHTML = tempDisplayed;
-
-  //   document.querySelector("#temp").innerHTML = Math.round(
-  //     response.data.main.temp
-  //   );
-
-  let temperatureElement = document.querySelector("#temp");
+  let citySearched = document.querySelector("#city-searched");
+  let temperatureShown = document.querySelector("#temperature");
   let tempDisplayed = Math.round(response.data.main.temp);
-  temperatureElement.innerHTML = `Temperature: ${tempDisplayed}℃`;
-  // console.log(tempDisplayed);
-  // debugger;
-  let windElement = document.querySelector("#wind");
-  windDisplayed = response.data.wind.speed;
-  windElement.innerHTML = `Wind: <i class="fa-solid fa-wind wind"></i>${windDisplayed} km/h`;
-  //   console.log(response.data.wind.speed);
+  temperatureShown.innerHTML = tempDisplayed;
+  celsiusTemperature = response.data.main.temp;
 
-  weatherDescriptionElement = document.querySelector("#weather-description");
-  descriptionDisplayed = response.data.weather[0].main;
-  weatherDescriptionElement.innerHTML = `<i class="fa-solid fa-cloud cloud"> ${descriptionDisplayed}`;
-  //   console.log(response.data.weather[0].main);
-  let humidityElement = document.querySelector("#humidity");
-  humidtyDisplayed = response.data.main.humidity;
-  humidityElement.innerHTML = `Humidity: ${humidtyDisplayed}%`;
+  let humidityShown = document.querySelector("#humidity");
+  let windShown = document.querySelector("#wind");
+  let weatherconditionShown = document.querySelector("#weather-condition");
+
+  citySearched.innerHTML = response.data.name;
+  temperatureShown.innerHTML = Math.round(response.data.main.temp);
+  humidityShown.innerHTML = response.data.main.humidity;
+  windShown.innerHTML = Math.round(response.data.wind.speed);
+  weatherconditionShown.innerHTML = response.data.weather[0].description;
+  // console.log(response.data.weather[0].icon);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-function searchCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  let apiKey = "5201594abea9f3e38b70e65b11a80c24";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+function search(city) {
+  // event.preventDefault();
+  // let city = document.querySelector("#city-input").value;
+  let apiKey = "eae061c95483dd066657bfc7525418ed";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let citySearched = document.querySelector("#city-input");
+  search(citySearched.value);
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchCity);
+searchForm.addEventListener("submit", handleSubmit);
 
-function getCurrentWeatherdetails(position) {
-  let apiKey = "5201594abea9f3e38b70e65b11a80c24";
+let celsiusTemperature = null;
+
+function displayCeltemp(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temperature");
+  // let celsiusTemp = document.querySelector("#celLink");
+  tempElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celLink = document.querySelector("#celLink");
+celLink.addEventListener("click", displayCeltemp);
+
+function displayFarhtemp(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temperature");
+  let farhtemp = (celsiusTemperature * 9) / 5 + 32;
+  tempElement.innerHTML = Math.round(farhtemp);
+}
+
+let farLink = document.querySelector("#farhLink");
+farLink.addEventListener("click", displayFarhtemp);
+
+function getCurrentLocationData(position) {
+  console.log(position.coords.latitude);
+  let apiKey = "eae061c95483dd066657bfc7525418ed";
   let units = "metric";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeather);
 }
 
-function showCurrentPosition(event) {
+function showPosition(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(getCurrentWeatherdetails);
+  navigator.geolocation.getCurrentPosition(getCurrentLocationData);
 }
 
-let currentLocationData = document.querySelector("#currentloc-btn");
-currentLocationData.addEventListener("click", showCurrentPosition);
+let currentLocationButton = document.querySelector("#current-locationbtn");
+currentLocationButton.addEventListener("click", showPosition);
+
+// SCRIPTS FOR CITIES I HOPE TO VISIT SOME DAY BELOW.
 
 function getOntarioWeather(event) {
   event.preventDefault();
-  let city = "ontario";
+  let apiKey = "5201594abea9f3e38b70e65b11a80c24";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=ontario&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getLucerneWeather(event) {
+  event.preventDefault();
+  let city = "lucerne";
   let apiKey = "5201594abea9f3e38b70e65b11a80c24";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
@@ -121,11 +159,8 @@ function getOntarioWeather(event) {
 
 function getMadeiraWeather(event) {
   event.preventDefault();
-
   let apiKey = "5201594abea9f3e38b70e65b11a80c24";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=madeira&appid=${apiKey}&units=metric`;
-
-  // `https://api.openweathermap.org/data/2.5/weather?q=madeira&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
 }
 
@@ -137,11 +172,17 @@ function getDaresalamWeather(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeather);
 }
+
 let ontarioWeather = document.querySelector("#ontario");
-ontarioWeather.addEventListener("click", getOntarioWeather);
+ontarioWeather.addEventListener("cliick", getOntarioWeather);
+
+let LucerneWeather = document.querySelector("#lucerne");
+LucerneWeather.addEventListener("cliick", getLucerneWeather);
 
 let madeiraWeather = document.querySelector("#madeira");
 madeiraWeather.addEventListener("click", getMadeiraWeather);
 
 let daresalamWeather = document.querySelector("#daresalam");
 daresalamWeather.addEventListener("click", getDaresalamWeather);
+
+search("Texas");
